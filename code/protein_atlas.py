871fdbd75784a40eb24739d5ebfaaa9f5db5fc4b
Alexander Umale <alexander.umale@gmail.com>
2019-02-24 22:38:52 -0500

    update prediction method

--- code/protein_atlas.py
+++ code/protein_atlas.py
@@ -357,31 +357,25 @@ def train(epochs=1000, **kwargs ):
 
 
 def make_predictions(**kwargs):
-	# TODO: write testing loop
 	# testing step
+	
 	with tf.device("/device:GPU:0"):
-		with tf.name_scope('test_loop'):
-			test_graph = get_graph( graph_type='test', summaries_dir=kwargs['te_summaries_dir'], **kwargs )
-			with test_graph['graph'].as_default():
-				with tf.variable_scope('test_graph'):	
-					te_iter = get_batch( img_file_path=kwargs['image_dir'], batch_type='test', lbl_dir_path=kwargs['label_dir'], batch_size=kwargs['test_batch_size'] )
-					te_img_ids, te_x, te_y = te_iter.get_next()
-					test_model = get_model(X=te_x, Y=te_y, mode='test', output_units=kwargs['label_size'])
-					all_predictions = []
-					# merge all summaries to be saved during training
-					te_merge_all_summaries = test_graph['summaries']
-					te_all_summaries = te_merge_all_summaries()
-					test_graph['saver'] = test_graph['saver']()
-					test_graph['saver'].restore(sess=test_graph['session'], save_path= tf.train.latest_checkpoint( checkpoint_dir=checkpoint_dir))
+			with tf.name_scope('test_loop'):
+				with graph.as_default():
+					with tf.variable_scope('test_graph'):    
+						te_iter = get_batch( img_file_path=kwargs['image_dir'], batch_type='test', lbl_dir_path=kwargs['label_dir'], batch_size=kwargs['test_batch_size'] )
+						te_img_ids, te_x, te_y = te_iter.get_next()
+						
+						graph.run(te_iter.initializer)
+						while True:
+							try:
+								#validate model
+								predictions = graph.run(Y_, {X:tr_x})
+								all_predictions.append( zip(te_img_ids,predictions) )
+							except tf.errors.OutOfRangeError:
+								with open(os.path.join(checkpoint_dir, 'predictions.csv')) as fp:
+									fp.write(all_predictions)
+								break
 
-					test_graph['session'].run(te_iter.initializer)
-					while True:
-						try:
-							#validate model
-							predictions =	test_graph['session'].run([test_model['prediction']])
-							all_predictions.append( zip(te_img_ids,predictions) )
-						except tf.errors.OutOfRangeError:
-							with open(os.path.join(checkpoint_dir, 'predictions.csv')) as fp:
-								fp.write(all_predictions)
-							break
+
 
